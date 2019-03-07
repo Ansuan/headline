f255b683b4a5d45c6a5bfb6b609c293233ff1d88
Antonio <antoniosimire2@gmail.com>
2019-03-07 14:21:11 +0100

    Upload proyecto1

---
+++ proyecto1/headlines.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+import feedparser
+from flask import render_template
+from lxml import etree
+#import urllib2
+from urllib.request import urlopen
+
+app= Flask(__name__)
+
+@app.route("/")
+def get_news():
+  news = []
+  url = 'http://ep00.epimg.net/rss/tags/ultimas_noticias.xml'
+  news = feedparser.parse(url)['entries'][:5]
+  return render_template("home.html", news=news, show_fotos=0, show_news=1)
+
+@app.route("/fotosMin")
+def get_miniature():
+  fotos = []
+  ns={"Atom" : "http://www.w3.org/2005/Atom"}
+  parser = etree.XMLParser()
+  tree = etree.parse(urlopen('https://api.flickr.com/services/feeds/photos_public.gne?tags=sevilla'),parser)
+  entries = tree.xpath('//Atom:entry', namespaces=ns) 
+  for i in entries:
+     unafoto = {}
+     unafoto['link'] = i.getchildren()[1].attrib['href']
+     unafoto['title'] = i.getchildren()[0].text
+     unafoto['src'] = i.getchildren()[9].attrib['href']
+     fotos.append(unafoto)
+  return render_template("home1.html", fotos=fotos)
+
+@app.route("/fotosSevilla")
+def get_fotos():
+  fotos = []
+  ns={"Atom" : "http://www.w3.org/2005/Atom"}
+  parser = etree.XMLParser()
+  tree = etree.parse(urlopen('https://api.flickr.com/services/feeds/photos_public.gne?tags=sevilla'),parser)
+  fotos = tree.xpath('//Atom:entry/Atom:title', namespaces=ns) 
+  return render_template("home.html", fotos=fotos, show_fotos=1, show_news=0)
+
+if __name__ == '__main__':
+  app.run(port=5300,debug=True)
+
+
